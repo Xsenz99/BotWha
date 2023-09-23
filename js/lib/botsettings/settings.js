d0a5b7089614c306d984a27c0b83a10a5d4638fe
Muhammad Yasin <94098776+Xsenz99@users.noreply.github.com>
2023-09-23 14:06:26 +0700

    Add files via upload

--- js/lib/botsettings/settings.js
+++ js/lib/botsettings/settings.js
@@ -1,1 +1,18 @@
+const fs = require('fs')
+const chalk = require('chalk')
+// EDIT DISINI
+global.owner = ['6281264116045'] // no own
+global.author = 'SENZ OFC' // nama author 
+global.packname = 'xsenz99' // nama pack sticker
+global.namabot = 'JAJAN FOLLOWERS' // nama bot mu
+global.group = 'https://chat.whatsapp.com/IJqUorim1oJ4kziHcf7xH5' // grup mu
+global.pic = 'https://f.top4top.io/p_2806ayrui0.png' // logo lu
 
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.yellowBright(`Update File Terbaru ${__filename}`))
+delete require.cache[file]
+require(file)
+})
